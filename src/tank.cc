cae4f718abaec6c4f63f66b60af4cc4a81172302
Katie <kjo@live.ca>
2016-04-18 09:01:48 +0200

    Finish tank class

--- src/tank.cc
+++ src/tank.cc
@@ -1,11 +1,30 @@
 #include "tank.h"
 
+using namespace Eigen;
+
+const double pi = 3.14159265358979323846;
+
 const Tank::TankInput Tank::default_input = 0.7;
 const Tank::TankState Tank::default_initial_state = 1.12;
 
 Tank::TankState Tank::GetDerivative(const TankState x,
-                                const TankInput u) const {
+                                    const TankInput u) const {
   TankState dxdt = x;
   return dxdt;
 }
 
+Tank::Params::Params() {
+  pout = 1;
+  volume = 20 * pi * (0.60 / 2) * (0.60 / 2) * 2 +
+           pi * (0.08 / 2) * (0.08 / 2) * 5.940;
+  D = (Eigen::Matrix<double, 8, 1>() << -0.0083454, -0.0094965, 0.16826, -0.032215,
+       -0.61199, 0.94175, -0.48522, 0.10369).finished();
+  m_out_c = 0.017;
+}
+
+Tank::Params::Params(const Params &x) {
+  pout = x.pout;
+  volume = x.volume;
+  D = x.D;
+  m_out_c = x.m_out_c;
+}
--- src/tank.h
+++ src/tank.h
@@ -12,20 +12,30 @@
  */
 class Tank {
  public:
+  typedef double TankInput;
+  typedef double TankState;
+
   const static int n_states = 1;
   const static int n_inputs = 1;
 
-  void operator()(const double &x_in, double &dxdt,
+  struct Params {
+    double pout;
+    double volume;
+    Eigen::Matrix<double,8,1> D;
+    double m_out_c;
+    Params();
+    Params(const Params &x);
+  };
+
+  void operator()(const TankState &x_in, TankState &dxdt,
                   const double /* t */) const {
   
     dxdt = GetDerivative(x_in, u);
   }
 
-  typedef double TankInput;
-  typedef double TankState;
-
   TankInput u;
   TankState x;
+  Params params;
 
   Tank(TankState xinit, TankInput uinit) : x(xinit), u(uinit) {}
   Tank() : x(default_initial_state), u(default_input) {}

8408ab340cf97104005f7f47c2c8e88563efc629
Katie <kjo@live.ca>
2016-04-29 20:26:18 +0200

    Document tank & make tank params private

--- src/tank.h
+++ src/tank.h
@@ -7,17 +7,21 @@
 
 #include "global_constants.h"
 
-/*
- * Class containing compressor tank
+/**
+ * Class containing tank used in compressor systems.
  */
 class Tank {
  public:
-  constexpr static int n_states = 1;
-  constexpr static int n_inputs = 1;
+  constexpr static int n_states = 1; ///< Number of tank states.
+  constexpr static int n_inputs = 1; ///< Number of tank inputs.
 
+  /// Type describing input to tank.
   typedef Eigen::Array<double, n_inputs, 1> TankInput;
+
+  /// Type describing state of tank.
   typedef Eigen::Array<double, n_states, 1> TankState;
 
+  /// Parameters determining dynamics of tank.
   struct Params {
     double pout;
     double volume;
@@ -25,20 +29,32 @@ class Tank {
     double m_out_c;
     Params();
     Params(const Params &x);
-  };
+  }; 
 
+  /// Specify parameters to use.
   Tank(Params params) : params(params) {}
+
+  /// Use default parameters.
   Tank() : params(Params()) {}
 
+  /**
+   * Get derivative of tank.
+   * Calculate derivative at state x and input u. Total mass flow into tank is given by mass_flow_compressors.
+   */
   TankState GetDerivative(const TankState x, const TankInput u,
                           const double mass_flow_compressors) const;
+
+  /// Return default input to tank.
   static inline TankInput GetDefaultInput() {
     return ((TankInput() << 0.7).finished());
   }
+  
+  /// Return default state of tank.
   static inline TankState GetDefaultState() {
     return ((TankState() << 1.12).finished());
   }
 
+ private:
   const Params params;
 };
 

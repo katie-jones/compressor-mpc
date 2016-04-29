988a96c04b68a117e549fcd24a6beae6ca42517d
Katie <kjo@live.ca>
2016-04-29 20:16:05 +0200

    Document compressor

--- src/compressor.h
+++ src/compressor.h
@@ -10,20 +10,27 @@
 // Forward declaration for friend class
 class ParallelCompressors;
 
-/*
- * Minimal implementation of single compressor
- * Only contains parameters as member variables. Input and state must always be specified.
+/**
+ * Minimal implementation of single compressor.
+ * Only contains parameters as member variables. Derivative can be calculated
+ * but input and state must always be specified.
  */
 class Comp {
  public:
-  constexpr static int n_states = 5;
-  constexpr static int n_inputs = 4;
-  constexpr static int n_outputs = 2;
+  constexpr static int n_states = 5;   ///< Number of states in model.
+  constexpr static int n_inputs = 4;   ///< Number of inputs to model.
+  constexpr static int n_outputs = 2;  ///< Number of outputs from model.
 
+  /// Type describing state of compressor.
   typedef Eigen::Array<double, n_states, 1> CompressorState;
+
+  /// Type describing inputs to compressor.
   typedef Eigen::Array<double, n_inputs, 1> CompressorInput;
+
+  /// Type describing outputs of compressor.
   typedef Vec<n_outputs> CompressorOutput;
 
+  /// Coefficients describing dynamics of compressor.
   struct Coefficients {
     double J, tau_r, m_in_c, m_out_c, torque_drive_c;
     Vec<8> C, D;
@@ -34,44 +41,60 @@ class Comp {
     Coefficients();
   };
 
+  /// Characteristics of fluid flow in compressor.
   struct FlowConstants {
     double a, Pin, Pout, V1, V2, AdivL;
     FlowConstants();
     FlowConstants(const FlowConstants &x);
   };
 
+  /// Initialize compressor with coefficients and flow parameters.
   Comp(Coefficients coeffs = Coefficients(),
        FlowConstants flow_constants = FlowConstants())
       : coeffs(coeffs), flow_constants(flow_constants) {}
 
+  /// Copy constructor.
   Comp(const Comp &x) : coeffs(x.coeffs), flow_constants(x.flow_constants) {}
 
+  /**
+   * Get derivative and mass flow of compressor about given operating point.
+   * Return derivative of compressor about state x, input u and pressure at the
+   * outlet pout. Also return mass flow through the compressor in variable
+   * m_out.
+   */
   CompressorState GetDerivative(const CompressorState x,
                                 const CompressorInput u, const double pout,
                                 double &m_out) const;
+
+  /**
+   * Get derivative of compressor about given operating point.
+   * Define a local variable to give as m_out argument to GetDerivative.
+   */
   inline CompressorState GetDerivative(const CompressorState x,
                                        const CompressorInput u,
                                        const double pout) const {
     double m_out = 0;
     return GetDerivative(x, u, pout, m_out);
   }
-  
+
+  /// Return output values of compressor for given state.
   CompressorOutput GetOutput(const CompressorState x) const;
 
+  /// Return default compressor state.
   const static inline CompressorState GetDefaultState() {
     return ((CompressorState() << 0.898, 1.126, 0.15, 440, 0).finished());
   }
+
+  /// Return default compressor input.
   const static inline CompressorInput GetDefaultInput() {
     return ((CompressorInput() << 0.304, 0.405, 0.393, 0).finished());
   }
 
-
  protected:
-  Coefficients coeffs;
-  FlowConstants flow_constants;
+  Coefficients coeffs; // parameters determining dynamics of compressor
+  FlowConstants flow_constants; // flow parameters
 
   friend ParallelCompressors;
 };
-
 
 #endif
--- src/compressor_simulation.h
+++ src/compressor_simulation.h
@@ -9,53 +9,78 @@
 #include "global_constants.h"
 #include "compressor.h"
 
-/*
- * Class containing single compressor
- * Overloaded () operator to give state derivative
+/**
+ * Child class of compressor implementing methods required for simulations.
+ * Contains member variables x (state) and u (inputs), overloaded () operator to
+ * give state derivative. Also defines a friend function to integrate the system
+ * over a given time range.
  */
 class CompressorSimulation : public Comp {
  public:
+   /// Function pointer used for callback when integrating system.
   typedef void (*IntegrationCallbackPtr)(const CompressorState, const double);
 
+  /// Default outlet pressure.
   constexpr static double default_pout = 1.0;
 
-  CompressorSimulation(CompressorState x = GetDefaultState(), CompressorInput u = GetDefaultInput(),
+  /// Provide initial member variable values or use defaults.
+  CompressorSimulation(CompressorState x = GetDefaultState(),
+                       CompressorInput u = GetDefaultInput(),
                        double pout = default_pout,
                        Coefficients coeffs = Coefficients(),
                        FlowConstants flow_constants = FlowConstants())
       : x(x), u(u), pout(pout), Comp(coeffs, flow_constants) {}
 
+  /**
+   * Calculate the derivative of the system.
+   * Override the () operator to calculate the derivative using the provided
+   * state x_in and the inputs given in the member variable u. Return the
+   * derivative in the second argument.
+   */
   void operator()(const CompressorState &x_in, CompressorState &dxdt,
                   const double /* t */) const {
     dxdt = GetDerivative(x_in, u, pout);
   }
 
+  /// Return output of system using current state (x)
   inline CompressorOutput GetOutput() { return Comp::GetOutput(x); }
 
-
+  /**
+   * Integrate compressor comp from t0 to tf with timestep dt.
+   * Integrate the compressor comp from its current state and input values at t0
+   * until time tf, using timestep dt. After each timestep the function provided
+   * in callback is called where, for example, the system inputs and other
+   * parameters can be changed. The integration method used is a variable-step
+   * Dormand-Prince algorithm which reduces the step size until the relative and
+   * absolute error are with the bounds specified in max_rel_error and
+   * max_abs_error respectively.
+   */
   friend void IntegrateCompressor(CompressorSimulation comp, const double t0,
                                   const double tf, const double dt,
                                   IntegrationCallbackPtr callback,
-                                  const double rel_error = 1e-6,
-                                  const double abs_error = 1e-6) {
+                                  const double max_rel_error = 1e-6,
+                                  const double max_abs_error = 1e-6) {
     ControlledStepper stepper =
-        make_controlled(rel_error, abs_error, Dopri5Stepper());
+        make_controlled(max_rel_error, max_abs_error, Dopri5Stepper());
     integrate_const(stepper, comp, comp.x, t0, tf, dt, callback);
   }
 
-  CompressorInput u;
-  CompressorState x;
-  double pout;
+  CompressorInput u; ///< Current inputs to compressor.
+  CompressorState x; ///< Current state of compressor.
+  double pout; ///< Pressure at the outlet of compressor.
 
  private:
+  // Type of stepper used to integrate
   typedef boost::numeric::odeint::runge_kutta_dopri5<
       CompressorState, double, CompressorState, double,
       boost::numeric::odeint::vector_space_algebra> Dopri5Stepper;
+
+  // Type of stepper used for variable step size integration
   typedef boost::numeric::odeint::controlled_runge_kutta<Dopri5Stepper>
       ControlledStepper;
 };
 
-// Define norm of Eigen::Array
+// Define norm of Eigen::Array for odeint to work
 namespace boost {
 namespace numeric {
 namespace odeint {

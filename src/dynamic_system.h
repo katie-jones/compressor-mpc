af6af26f0d862e91b453f5c6b35b26a9f732991f
Katie <kjo@live.ca>
2016-05-12 22:37:17 +0200

    Write print_matrix function in separate file

--- src/dynamic_system.h
+++ src/dynamic_system.h
@@ -2,8 +2,7 @@
 #define DYNAMIC_SYSTEM_H
 
 #include <Eigen/Eigen>
-#include <iostream>
-#include <iomanip>
+#include "print_matrix.h"
 
 // class Observer;
 
@@ -44,20 +43,6 @@ class DynamicSystem {
     }
 
    private:
-    template <typename Derived>
-    static std::ostream& print_matrix(std::ostream& os,
-                                      const Eigen::MatrixBase<Derived>& x,
-                                      const std::string name) {
-      os << name << " = " << std::endl;
-      for (int i = 0; i < x.rows(); i++) {
-        for (int j = 0; j < x.cols(); j++) {
-          os << std::setprecision(3) << std::setw(10) << x(i, j) << " ";
-        }
-        os << std::endl;
-      }
-      os << std::endl;
-      return os;
-    }
   };
 
   virtual ~DynamicSystem() {}

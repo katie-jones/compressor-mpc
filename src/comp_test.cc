46d6d9fb00eaa7b2df018225f0dc0179f72b9f78
Katie <kjo@live.ca>
2016-05-17 13:08:56 +0200

    Add basic test of MpcController

--- src/comp_test.cc
+++ src/comp_test.cc
@@ -6,9 +6,17 @@
 #include "parallel_compressors.h"
 #include "simulation_parallel_compressors.h"
 #include "mpc_controller.h"
+#include "print_matrix.h"
 
-// extern template class MpcController<Compressor, 2, 3,5,2>;
-// using Controller = MpcController<Compressor, 2, 3,5,2>;
+extern template class MpcController<Compressor, 2, 2, 5, 2>;
+using Controller = MpcController<Compressor, 2, 2, 5, 2>;
+
+namespace Control {
+constexpr int n_delay_states = 2;
+constexpr int n_disturbance_states = 2;
+constexpr int p = 5;
+constexpr int m = 2;
+}
 
 void Callback(Compressor::State x, double t) {
   std::cout << "t: " << t << "\tx: ";
@@ -24,11 +32,11 @@ void CallbackSys(ParallelCompressors::State x, double t) {
 }
 
 int main(void) {
-  // Compressor x;
+  Compressor x;
 
   // Compressor::State xin = Compressor::GetDefaultState();
   // Compressor::Linearized linsys =
-      // x.GetLinearizedSystem(xin, Compressor::GetDefaultInput());
+  // x.GetLinearizedSystem(xin, Compressor::GetDefaultInput());
 
   // std::cout << xin(0) << std::endl;
   // std::cout << linsys.A(0, 0) << std::endl;
@@ -47,8 +55,8 @@ int main(void) {
 
   // ParallelCompressors parcomp = ParallelCompressors();
   // ParallelCompressors::Linearized plin =
-      // parcomp.GetLinearizedSystem(ParallelCompressors::GetDefaultState(),
-                                  // ParallelCompressors::GetDefaultInput());
+  // parcomp.GetLinearizedSystem(ParallelCompressors::GetDefaultState(),
+  // ParallelCompressors::GetDefaultInput());
   // std::cout << plin.A(10, 1) << std::endl;
 
   // std::ifstream finaltime("finaltime.txt");
@@ -57,25 +65,41 @@ int main(void) {
   // SimulationParallelCompressors compsys = SimulationParallelCompressors();
   // compsys.Integrate(0, tf, 0.05, CallbackSys);
 
-
-
-
   // std::array<int, 2> n_delay = {0, 3};
   // std::array<int, 2> control_input_index = {0, 3};
 
-  // std::array<double, 2*2*2*3*3> A = MpcController<Compressor, 4, 2, 20, 3>::GetConstraintMatrix();
+  // std::array<double, 2*2*2*3*3> A = MpcController<Compressor, 4, 2, 20,
+  // 3>::GetConstraintMatrix();
 
   // std::cout << "A = " << std::endl;
   // for (int i=0; i<2*3*2; i++) {
-    // for (int j=0; j<2*3; j++) {
-      // std::cout << A[j+6*i] << "\t";
-    // }
-    // std::cout << std::endl;
+  // for (int j=0; j<2*3; j++) {
+  // std::cout << A[j+6*i] << "\t";
+  // }
+  // std::cout << std::endl;
   // }
 
   // Controller ctrl(augsys);
   // Controller::Prediction pred = ctrl.GetPredictionMatrices();
+  const Controller::ObserverMatrix M =
+      (Controller::ObserverMatrix()
+           << Eigen::Matrix<double, x.n_states,
+                            x.n_control_inputs>::Zero(),
+       Eigen::Matrix<double, Control::n_delay_states,
+                     Control::n_delay_states>::Identity()).finished();
+  const Controller::ControlInputIndex delay = {0, 2};
+  const Controller::ControlInputIndex index = {0, 3};
 
+  Controller::InputConstraints constraints;
+  constraints.lower_bound << -0.3, 0;
+  constraints.upper_bound << 0.3, 1;
+  constraints.lower_rate_bound << -0.1, -0.1;
+  constraints.upper_rate_bound << 0.1, 1;
+
+  Controller ctrl(x, M, 0.05, delay, index);
+  ctrl.SetInitialState(x.GetDefaultState(), Controller::ControlInput::Zero());
+  Compressor::Input u = ctrl.GetNextInput(Compressor::Output::Zero());
+  print_matrix(std::cout, u.matrix(), "u");
 
   return 0;
 }

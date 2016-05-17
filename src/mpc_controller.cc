959d7691bcad88ef617624638a3a49fd0eb796ac
Katie <kjo@live.ca>
2016-05-17 13:00:09 +0200

    Replicate constraints m times

--- src/mpc_controller.cc
+++ src/mpc_controller.cc
@@ -205,10 +205,19 @@ MpcController<System, n_delay_states, n_disturbance_states, p, m>::SolveQP(
     const QP& qp) {
   int n_wsr = n_wsr_max;
 
-  qpOASES::returnValue status = qp_problem_.hotstart(
-      qp.H.data(), qp.f.data(), Ain_.data(), u_constraints_.lower_bound.data(),
-      u_constraints_.upper_bound.data(), u_constraints_.upper_rate_bound.data(),
-      u_constraints_.lower_rate_bound.data(), n_wsr, NULL);
+  // Replicate constraints for number of moves
+  const Eigen::Matrix<double, m * n_control_inputs, 1> lb =
+      u_constraints_.lower_bound.template replicate<m, 1>();
+  const Eigen::Matrix<double, m * n_control_inputs, 1> ub =
+      u_constraints_.upper_bound.template replicate<m, 1>();
+  const Eigen::Matrix<double, m * n_control_inputs, 1> lbA =
+      u_constraints_.lower_rate_bound.template replicate<m, 1>();
+  const Eigen::Matrix<double, m * n_control_inputs, 1> ubA =
+      u_constraints_.upper_rate_bound.template replicate<m, 1>();
+
+  qpOASES::returnValue status =
+      qp_problem_.hotstart(qp.H.data(), qp.f.data(), Ain_.data(), lb.data(),
+                           ub.data(), lbA.data(), ubA.data(), n_wsr, NULL);
 
   if (status != qpOASES::SUCCESSFUL_RETURN) {
     // QP not solved, return zeros

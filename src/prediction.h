4698bc8e8d1f702263720f3fa871d81a6e13ba76
Katie <kjo@live.ca>
2016-06-09 21:18:34 +0200

    Add Su_other to prediction

--- src/prediction.h
+++ src/prediction.h
@@ -5,11 +5,12 @@
 
 /// Linearized prediction of dynamic system
 struct Prediction {
-  Eigen::MatrixXd Su, Sx, Sf;
-  Prediction() : Su(), Sx(), Sf() {}
+  Eigen::MatrixXd Su, Sx, Sf, Su_other;
+  Prediction() : Su(), Sx(), Sf(), Su_other() {}
   Prediction(const Eigen::MatrixXd& Su_in, const Eigen::MatrixXd& Sx_in,
-             const Eigen::MatrixXd& Sf_in)
-      : Su(Su_in), Sx(Sx_in), Sf(Sf_in) {}
+             const Eigen::MatrixXd& Sf_in,
+             const Eigen::MatrixXd& Su_other_in = Eigen::MatrixXd())
+      : Su(Su_in), Sx(Sx_in), Sf(Sf_in), Su_other(Su_other_in) {}
 };
 
 #endif
